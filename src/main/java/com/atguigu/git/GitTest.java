8af8166fea90955df807b6e5478a401ea970f454
Lord-Lin <116432191+Lord-Lin@users.noreply.github.com>
2022-10-23 21:02:38 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,9 +1,6 @@
 package com.atguigu.git;
 
-/**
- * @author 林柳梅
- * @version 1.000
- */
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git 1~");
